23c6602d13b9cab9d2de64e2ee590af540c97863
Azeem <azeemahmed101@gmail.com>
2020-05-23 18:34:37 +0200

    add necessary quit for driver

--- bin/main.rb
+++ bin/main.rb
@@ -21,27 +21,30 @@ i = 0
 j = 0
 
 progressbar = ProgressBar.create(title: 'Books', total: 55, length: 80, format: '%a <%B> %p%% %t')
+begin
+  driver = Selenium::WebDriver.for :chrome
+  wait = Selenium::WebDriver::Wait.new(timeout: 10)
+  driver.get('https://goodreads.com')
 
-driver = Selenium::WebDriver.for :chrome
-wait = Selenium::WebDriver::Wait.new(timeout: 10)
-driver.get('https://goodreads.com')
-
-NYTPAGE_CONST.each do |section|
-  5.times do
-    book = BookData.new(i, section).book_data_array
-    i += 1
-    all_books << book
-    until j == all_books.count
-      GReadsScraper.new(j, all_books, driver).scrap_goodreads
-      rating = Ratings.new(driver)
-      avg_rating = wait.until { rating.avg_rating }
-      num_of_ratings = wait.until { rating.num_of_ratings }
-      book.insert(4, avg_rating)
-      book.insert(5, num_of_ratings)
-      GReadsScraper.new(driver).reset_browser
-      j += 1
-      progressbar.increment
+  NYTPAGE_CONST.each do |section|
+    5.times do
+      book = BookData.new(i, section).book_data_array
+      i += 1
+      all_books << book
+      until j == all_books.count
+        GReadsScraper.new(j, all_books, driver).scrap_goodreads
+        rating = Ratings.new(driver)
+        avg_rating = wait.until { rating.avg_rating }
+        num_of_ratings = wait.until { rating.num_of_ratings }
+        book.insert(4, avg_rating)
+        book.insert(5, num_of_ratings)
+        GReadsScraper.new(driver).reset_browser
+        j += 1
+        progressbar.increment
+      end
+      books_csv << book
     end
-    books_csv << book
   end
+ensure 
+  driver.quit
 end
